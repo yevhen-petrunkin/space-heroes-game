02b558476c63b79483a03d68f39ca5589b1f62d7
yevhen-petrunkin <novamovaxxi@gmail.comm>
2024-10-01 02:08:11 +0300

    CMT#0006 Add button ui component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,9 @@
+import { Button } from '@/game-engine/components/ui/button';
 // eslint-disable-next-line react/function-component-definition
 export default function Home() {
-    return <main>Space Heroes</main>;
+    return (
+        <main>
+            <Button>Button</Button>
+        </main>
+    );
 }
