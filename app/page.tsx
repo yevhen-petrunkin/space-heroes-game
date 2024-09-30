e2967b8382f7bfabd2b4dbbf155b6e73245b5509
yevhen-petrunkin <novamovaxxi@gmail.comm>
2024-10-01 02:16:47 +0300

    CMT#0007 Add badge ui component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Badge } from '@/game-engine/components/ui/badge';
 import { Button } from '@/game-engine/components/ui/button';
 // eslint-disable-next-line react/function-component-definition
 export default function Home() {
     return (
         <main>
             <Button>Button</Button>
+            <Badge>Badge</Badge>
         </main>
     );
 }
